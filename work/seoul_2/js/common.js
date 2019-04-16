361419d4cc9621f6b15e0321ba5d7f2e3741d920
johyukrae <johr0712@gamil.com>
2019-04-16 12:06:55 +0900

    asdas
    dasd

--- work/seoul_2/js/common.js
+++ work/seoul_2/js/common.js
@@ -6,17 +6,36 @@ $(function () {
         $('.wrapper').toggleClass('sideNav-open');
     });
 
-    $('.footer .top a').click(function(){
+    $('.footer .top a').click(function () {
         $('.footer .middle').toggleClass('on');
     })
-    $('.sideNav .menu>li').hover(function(){
+    $('.sideNav .menu>li').hover(function () {
         $('.sideNav .menu>li').removeClass('on');
         $(this).toggleClass('on');
     })
-    $('.sub .sideNav .menu>li').hover(function(){
+    $('.sub .sideNav .menu>li , .sideNav_2').hover(function () {
         $('.wrapper').toggleClass('on');
     })
+    $('.sideNav .menu>li , .sideNav_2').hover(function () {
+        $('.sideNav_2').toggleClass('on');
+    })
 
+    $('.gnb_target_1').hover(function () {
+        $('.drop').removeClass('on');
+        $('.gnb_target_1_hover').addClass('on');
+    })
+    $('.gnb_target_2').hover(function () {
+        $('.drop').removeClass('on');
+        $('.gnb_target_2_hover').addClass('on');
+    })
+    $('.gnb_target_3').hover(function () {
+        $('.drop').removeClass('on');
+        $('.gnb_target_3_hover').addClass('on');
+    })
+    $('.gnb_target_4').hover(function () {
+        $('.drop').removeClass('on');
+        $('.gnb_target_4_hover').addClass('on');
+    })
     $(".banner1").click(function () {
         $(this).parent().find(".banner2").animate({
             "left": "890px"
@@ -24,7 +43,6 @@ $(function () {
         $(this).parent().find(".banner3").animate({
             "left": "1035px"
         });
-
     });
     $(".banner2").click(function () {
         $(this).animate({
