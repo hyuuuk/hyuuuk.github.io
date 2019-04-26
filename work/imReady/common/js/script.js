4367091cb028c0c63fae11803b0d0328cdb51734
johyukrae <johr0712@gamil.com>
2019-04-26 23:31:17 +0900

    as
    dasd

--- work/imReady/common/js/script.js
+++ work/imReady/common/js/script.js
@@ -1,2 +1,28 @@
 jQuery(document).ready(function() {
+    $('.tab-header').each(function (element) {
+        var slider_width,
+            tab_width,
+            left_position,
+            $active,
+            $content,
+            $links = $(this).find('.tab-list'),
+            $currentTab = $(this).find('a.on')
+
+        $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+        $active.addClass('on');
+        $content = $($active[0].hash);
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+        // Binds the click event handler
+        $(this).on('click', 'a', function (e) {
+            $active.removeClass('on');
+            $content.hide();
+            $active = $(this);
+            $content = $(this.hash);
+            $active.addClass('on');
+            $content.show();
+            e.preventDefault();
+        });
+    });
 });
