60fddb2d854481ac5c9beef78e62e358e46062e0
johyukrae <johr0712@gamil.com>
2019-04-30 20:54:32 +0900

    asd
    asd

--- work/jungheung/js/script.js
+++ work/jungheung/js/script.js
@@ -5,5 +5,39 @@ $(function () {
     $('.btn-menu-close').click(function () {
         $('body').removeClass('open-menu');
     });
-    $("#datepicker").datepicker();
+    $('.btn-cost').click(function () {
+        $('.sub-con4').toggleClass('on');
+    });
+    $('.tab-header').each(function (element) {
+        var slider_width,
+            tab_width,
+            left_position,
+            $active,
+            $content,
+            $links = $(this).find('.tab-list'),
+            $currentTab = $(this).find('a.on')
+
+        $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+        $active.addClass('on');
+        $content = $($active[0].hash);
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+        $(this).on('click', 'a', function (e) {
+            $active.removeClass('on');
+            $content.hide();
+            $active = $(this);
+            $content = $(this.hash);
+            $active.addClass('on');
+            $content.show();
+            e.preventDefault();
+        });
+    });
+
+    $('.btn-scroll-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 });
