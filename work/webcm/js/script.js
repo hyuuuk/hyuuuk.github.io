8066b348df22f90ea1f55b7aea2d95841f580258
johyukrae <johr0712@gamil.com>
2019-05-03 22:10:10 +0900

    gfh
    fghfgh

--- work/webcm/js/script.js
+++ work/webcm/js/script.js
@@ -24,4 +24,33 @@ $(function () {
             e.preventDefault();
         });
     });
+    if (navigator.userAgent.match(/Trident\/7\./)) {
+        $('body').on("mousewheel", function () {
+            event.preventDefault();
+
+            var wheelDelta = event.wheelDelta;
+
+            var currentScrollPosition = window.pageYOffset;
+            window.scrollTo(0, currentScrollPosition - wheelDelta);
+        });
+
+        $('body').keydown(function (e) {
+            e.preventDefault(); // prevent the default action (scroll / move caret)
+            var currentScrollPosition = window.pageYOffset;
+
+            switch (e.which) {
+
+                case 38: // up
+                    window.scrollTo(0, currentScrollPosition - 120);
+                    break;
+
+                case 40: // down
+                    window.scrollTo(0, currentScrollPosition + 120);
+                    break;
+
+                default:
+                    return; // exit this handler for other keys
+            }
+        });
+    }
 });
