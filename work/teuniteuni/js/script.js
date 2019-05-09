1ff53cb7fc992598039ad72a3c902159152461dd
johyukrae <johr0712@gamil.com>
2019-05-09 14:09:26 +0900

    a
    a

--- work/teuniteuni/js/script.js
+++ work/teuniteuni/js/script.js
@@ -1,27 +1,6 @@
 $(function () {
-    $('.tab-header').each(function (element) {
-        var slider_width,
-            tab_width,
-            left_position,
-            $active,
-            $content,
-            $links = $(this).find('.tab-list'),
-            $currentTab = $(this).find('a.on')
-
-        $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
-        $active.addClass('on');
-        $content = $($active[0].hash);
-        $links.not($active).each(function () {
-            $(this.hash).hide();
-        });
-        $(this).on('click', 'a', function (e) {
-            $active.removeClass('on');
-            $content.hide();
-            $active = $(this);
-            $content = $(this.hash);
-            $active.addClass('on');
-            $content.show();
-            e.preventDefault();
-        });
+    $(".url").click(function () {
+        $(".url").removeClass("on");
+        $(this).addClass("on");
     });
 });
