9c7670af22c80b6992a402a6c22c9e770d10fe20
johyukrae <johr0712@gamil.com>
2019-05-11 18:27:02 +0900

    asd
    asd

---
+++ work/barunn/js/script.js
@@ -0,0 +1,34 @@
+$(function () {
+    $('.tab-header').each(function (element) {
+        var slider_width,
+            tab_width,
+            left_position,
+            $active,
+            $content,
+            $links = $(this).find('.tab-list'),
+            $currentTab = $(this).find('a.on')
+
+        $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+        $active.addClass('on');
+        $content = $($active[0].hash);
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+        $(this).on('click', 'a', function (e) {
+            $active.removeClass('on');
+            $content.hide();
+            $active = $(this);
+            $content = $(this.hash);
+            $active.addClass('on');
+            $content.show();
+            e.preventDefault();
+        });
+    });
+    $(".pay01 a").click(function () {
+        $(".pay01 a").removeClass("bg-blue");
+        $(this).addClass("bg-blue");
+    });
+    $(".sub01-title").click(function () {
+        $(this).next().toggleClass("active");
+    });
+});
