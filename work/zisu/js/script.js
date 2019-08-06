e133699f4c72e2bc5e2addeac6524f433730c1b8
hyuuuk <49545407+hyuuuk@users.noreply.github.com>
2019-08-06 21:43:03 +0900

    asd
    sad

---
+++ work/zisu/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.btn-menu').click(function(){
+        $('.m-menu').addClass('on');
+    })
+    $('.btn-close').click(function(){
+        $('.m-menu').removeClass('on');
+    })
+});
